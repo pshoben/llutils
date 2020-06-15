42e49ac19a989f4f33b747174bcbeeab8db3b9d6
pshoben <pshoben@yahoo.com>
2020-06-15 01:54:00 -0400

    add googletests

--- include/llutils.hpp
+++ include/llutils.hpp
@@ -1,2 +1,22 @@
+#ifndef llutils_hpp
+#define llutils_hpp
+
 #define llutils_VERSION_MAJOR 0
 #define llutils_VERSION_MINOR 1
+
+template <typename T>
+class LLUtils
+{
+public:
+	static T unsigned_cstr_to_num( const char *buffer, size_t len )
+	{
+		T result = 0;
+		while (len--) {
+			result = 10 * result + (*buffer++ - '0');
+		}
+		return result;
+	}       
+
+};
+
+#endif

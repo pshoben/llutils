34c42cbb5452a8d6d687e1e97f19a8b66c46108c
pshoben <pshoben@yahoo.com>
2020-06-14 20:56:13 -0400

    initial folder structure (wip)

---
+++ include/llutils.hpp
@@ -0,0 +1,2 @@
+#define llutils_VERSION_MAJOR 0
+#define llutils_VERSION_MINOR 1
---
+++ test/test_llutils.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <llutils.hpp>
+
+int main() 
+{
+  std::cout << "llutils Version " << llutils_VERSION_MAJOR << "."  << llutils_VERSION_MINOR << std::endl;
+}
+

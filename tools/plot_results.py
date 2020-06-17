9d8e164fabaa59722a1f75b67ca0008df066db2a
pshoben <pshoben@yahoo.com>
2020-06-17 12:33:51 -0400

    fix plot

--- tools/plot_results.py
+++ tools/plot_results.py
@@ -100,14 +100,15 @@ def plot_series(data,row_count,col_count,series_count,series_title, axs):
             arr = point[0].split("/")
             try:
                 y_value = int(point[3]) # cpu time
-                y_values.append(y_value)
-                if len(arr)>1:
-                    x_value = int(arr[1])
-                    x_values.append(x_value)
+                if not y_value == 0:
+                    y_values.append(y_value)
+                    if len(arr)>1:
+                        x_value = int(arr[1])
+                        x_values.append(x_value)
             except ValueError:
                 pass
         if len(x_values)>0 and len(y_values)>0 and len(x_values)==len(y_values):
-            handle, = axs[row_count].plot(np.array(x_values), np.array(y_values), label=series_title, marker='o', linestyle='--')
+            handle, = axs[row_count].plot(np.array(x_values), np.array(y_values), label=series_title, marker='.', linestyle='--')
 
 def main():
     plot_benchmark_results()

5f48c1502705a2cdf767374ba1f1461f81e13210
root <root@Unknown-2c-ea-7f-51-fe-d3.lan>
2020-09-01 06:17:12 -0400

    config4cpp changes

--- third_party/clone_dependencies.sh
+++ third_party/clone_dependencies.sh
@@ -2,5 +2,5 @@ git clone https://github.com/google/googletest.git
 git clone https://github.com/google/benchmark.git
 # Benchmark requires Google Test as a dependency. Add the source tree as a subdirectory.
 git clone https://github.com/google/googletest.git benchmark/googletest
-wget http://www.config4star.org/download/config4cpp.tar.gz
-tar -zxvf config4cpp.tar.gz
+#wget http://www.config4star.org/download/config4cpp.tar.gz
+#tar -zxvf config4cpp.tar.gz
